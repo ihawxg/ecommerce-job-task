bf7fd528f9650358daf6cc0a52cce59996501822
iHawx <ihawxg@gmail.com>
2023-08-07 13:55:14 +0300

    Header, footer added started working on product card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import './reset.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Header></Header>
+        <Main></Main>
+        <Routes>
+          <Route path ="/shoes"></Route>
+          <Route path ="/bags"></Route>
+          <Route path ="/t-shirts" ></Route>
+          <Route path ="/jeans" ></Route>
+          <Route path ="/watches"></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
