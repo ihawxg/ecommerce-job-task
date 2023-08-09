e28e625cdc55762e3afbddb53aea189893ce150a
iHawx <ihawxg@gmail.com>
2023-08-09 15:49:18 +0300

    added pagehero for about page and checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import PageHero from '../Components/PageHero'
+import styled from 'styled-components';
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
+  return <main>
+    <PageHero title='checkout'></PageHero>
+    <Wrapper className='page'>
+      <h1>checkout page</h1>
+    </Wrapper>
+    </main>
 }
+
+const Wrapper = styled.div``
 
 export default CheckoutPage
