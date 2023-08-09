7bdb207459d1c2491ce2656cdfd1d5961eef309a
iHawx <ihawxg@gmail.com>
2023-08-09 18:19:32 +0300

    added services component and contact form component, almost done with most of the visual stuff (everything is responsive up to now)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Home from '../Components/Home'
+import Services from '../Components/Services'
+import Contact from '../Components/Contact'
 
 
 
 const HomePage = () => {
   return <main>
     <Home></Home>
+    <Services></Services>
+    <Contact/>
+
     {/* <FeaturedProducts></FeaturedProducts>
     <Services></Services>
     <Contant></Contant> */}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
+import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+
+
 export const links = [
   {
     id: 1,
@@ -13,6 +16,30 @@ export const links = [
     id: 3,
     text: 'products',
     url: '/products',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCompass />,
+    title: 'mission',
+    text:
+      'To help my creator land a job as a front-end developer',
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard />,
+    title: 'vision',
+    text:
+      'To become good developer',
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote />,
+    title: 'history',
+    text:
+      'To be made',
   },
 ]
 
