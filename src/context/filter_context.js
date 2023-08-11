f14de00fd4e54ecf998f72eb6f6772436b5794bf
iHawx <ihawxg@gmail.com>
2023-08-11 14:06:49 +0300

    sort functionality added

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -2,14 +2,14 @@ import React, { useContext, useEffect } from "react";
 import { useReducer } from "react";
 import reducer from '../reducers/filter_reducer'
 import { useProductsContext } from "./products_context";
-import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW } from "../actions";
+import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW, SORT_PRODUCTS, UPDATE_SORT } from "../actions";
 
 
 const initialState={
     filtered_products:[],
     all_products:[],
     grid_view: true,
-
+    sort: 'price-lowest'
 }
 
 
@@ -24,6 +24,10 @@ export const FilterProvider = ({children})=>{
         dispatch({type:LOAD_PRODUCTS,payload:products})
     },[products])
 
+    useEffect(()=>{
+        dispatch({type:SORT_PRODUCTS})
+    },[products,state.sort])
+
     const setGridView = () =>{
         dispatch({type:SET_GRIDVIEW})
     }
@@ -32,7 +36,13 @@ export const FilterProvider = ({children})=>{
         dispatch({type:SET_LISTVIEW})
     }
 
-    return( <FilterContext.Provider value={{...state,setGridView,setListView}}>
+    const updateSort = (e)=>{
+        
+        const value = e.target.value;
+        dispatch({type:UPDATE_SORT,payload:value})
+    }
+
+    return( <FilterContext.Provider value={{...state,setGridView,setListView,updateSort}}>
         {children}
     </FilterContext.Provider>)
 }
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,15 +1,40 @@
-import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW } from "../actions"
+/* eslint-disable default-case */
+import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW, SORT_PRODUCTS, UPDATE_SORT } from "../actions"
 
-const filter_reducer=(state,action)=>{
-    if(action.type === LOAD_PRODUCTS){
-        return {...state, all_products:[...action.payload],filtered_products:[...action.payload]}
+const filter_reducer = (state, action) => {
+    if (action.type === LOAD_PRODUCTS) {
+        return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] }
     }
 
-    if(action.type === SET_GRIDVIEW){
-        return {...state,grid_view:true}
+    if (action.type === SET_GRIDVIEW) {
+        return { ...state, grid_view: true }
     }
-    if(action.type === SET_LISTVIEW){
-        return {...state,grid_view:false}
+    if (action.type === SET_LISTVIEW) {
+        return { ...state, grid_view: false }
+    }
+    if (action.type === UPDATE_SORT) {
+        return { ...state, sort: action.payload }
+    }
+    if (action.type === SORT_PRODUCTS) {
+        const { sort, filtered_products } = state;
+        let tempProducts = [...filtered_products];
+
+        switch (sort) {
+            case "price-lowest":
+                tempProducts = tempProducts.sort((a,b)=>a.price-b.price )
+                break;
+            case "price-highest":
+                tempProducts = tempProducts.sort((a,b)=>b.price-a.price )
+                break;
+            case "name-a":
+                tempProducts = tempProducts.sort((a,b)=>a.name.localeCompare(b.name))
+                break;
+            case "name-z":
+                tempProducts = tempProducts.sort((a,b)=>b.name.localeCompare(a.name))
+                break;
+        }
+
+        return { ...state,filtered_products:tempProducts }
     }
     return state;
 }
