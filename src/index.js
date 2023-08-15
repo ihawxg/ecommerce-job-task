f2556cf719e2b6658e824c827b0f075a10004759
iHawx <ihawxg@gmail.com>
2023-08-15 17:54:34 +0300

    auth0 authorizationParams fixed

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    redirectUri = {window.location.origin}
+    authorizationParams={{
+      redirect_uri: `${window.location.origin}/cart`,
+    }}
     cacheLocation='localstorage'
   >
     <React.StrictMode>
